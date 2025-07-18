6c92d8a654cb0111c483af16f414716b1e3f0cd1
Swadeshit27 <swadeshpal2002@gmail.com>
2025-07-18 22:36:25 +0530

    fixed image not showing issue

--- src/appwrite/file.ts
+++ src/appwrite/file.ts
@@ -43,7 +43,7 @@ export class FileService {
     }
 
     getFilePreview(fileId: string) {
-        return this.bucket.getFilePreview(
+        return this.bucket.getFileView(
             conf.appwriteBucketId,
             fileId
         )
